f06c3434c1c10b01c2209ee73e4b28801c307421
tarun.sharma4 <9953247589>
2020-07-04 18:21:39 +0530

    level one circle file

--- app/src/main/java/com/android/custom/view/CircleView.kt
+++ app/src/main/java/com/android/custom/view/CircleView.kt
@@ -9,39 +9,17 @@ import android.view.View
 import androidx.core.content.ContextCompat
 import com.android.retrofit.R
 
-
-class CircleView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
-    var paint: Paint = Paint()
-    var isCenter = false
-    var centerOfX = 0F
-    var centerOfY = 0F
-    var radiusOfCircleView = 0F
-
+class CircleView (context:Context): View(context){
+    var paint:Paint = Paint()
+    var centerOfX =  340F
+    var centerOfY =  340F
+    var radiusOfCircleView =  140F
     init {
-        var attributeArray: TypedArray? = context.theme.obtainStyledAttributes(
-            attributeSet,
-            R.styleable.circleview, 0, 0
-        )
-        paint.color = attributeArray?.getColor(
-            R.styleable.circleview_circle_background,
-            ContextCompat.getColor(context, android.R.color.background_dark)
-        ) ?: android.R.color.black
-        isCenter = attributeArray?.getBoolean(R.styleable.circleview_onCenter, false) ?: false;
-
-        radiusOfCircleView =
-            attributeArray?.getDimension(R.styleable.circleview_circle_radius, 140F) ?: 140F
+        paint.color = ContextCompat.getColor(context,android.R.color.holo_green_light)
         paint.strokeWidth = 40f
-        paint.style = Paint.Style.STROKE
-
-        // Constructor Call
-    }
-
-    override fun onDraw(canvas: Canvas?) {
-        isCenter?.let {
-            centerOfX = (width / 2).toFloat()
-            centerOfY = (height / 2).toFloat()
-        }
-        canvas?.drawCircle(centerOfX, centerOfY, radiusOfCircleView, paint)
+        paint.style = Paint.Style.STROKE// Constructor Call
+    }override fun onDraw(canvas: Canvas?) {
+        canvas?.drawCircle(centerOfX,centerOfY,radiusOfCircleView,paint)
         super.onDraw(canvas)
     }
 }
